e5c8f09f8f889b7fbcb0abc3c508f2872b15d406
BMan-21 <bodhibhatia@gmail.com>
2025-09-17 20:50:44 -0700

    Update card-gaem2.py

--- card-gaem2.py
+++ card-gaem2.py
@@ -27,7 +27,7 @@ blackbg = "\x1b[40m"
 reset = "\x1b[0m"
 print(red + "Welcome" + orange + " to" + yellow + " card" + green + " gaem" + blue + " 2" + reset)
 print("nothing yet!")
-money = 10
+money = 100
 binder = []
 def packanim(color):
     print(color)
@@ -315,75 +315,118 @@ def packanim(color):
     print("|              |")
     print("|______________|")    
 while True:
-    print("you have " + str(money) + " money")
-    menu = input("1. shop - 2. binder - 3. shop (no shop yet so dont type 3 ok?, i warned you): ")
+    print("you have " + yellow + str(money) + reset + " money")
+    menu = input("1. shop - 2. binder: ")
     if menu == "1":
-        pack = input("what pack do you want to open? 1. base pack (10 money), you have" + str(money) + "money: ")
-        if pack == "1":
-            #base pack
-            if money >= 10:
-                money -= 10
-            #(v) pack stuff
-                print("opening pack...")
-                #(v) find cards
-                card1 = randint(1,100)
-                card2 = randint(1,100)
-                cards = [card1, card2]
-                colors = []
-                for c in cards:
-                    #(v) common (75%)
-                    if c <= 75:
-                        binder.append(blue + "common")
-                        colors.append(blue)
-                    #(v) uncommon (25%)
-                    elif c > 75 and c <= 100:
-                        binder.append(green + "uncommon")
-                        colors.append(green)
-                #card animation
-                print(card1)
-                packanim(reset)
-                sleep(0.1)
-                print(colors[1] + " ______________ ")
-                print(colors[1] + "/" + colors[0] + "______________" + colors[1] + "\\")
-                print(colors[0] + "/" + reset + "______________" + colors[0] + "\\")
-                print(reset + "|              |")
-                print("|              |")
-                print("|              |")
-                print("|              |")
-                print("|              |")
-                print("|              |")
-                print("|              |")
-                print("|              |")
-                print("|              |")
-                print("|              |")
-                print("|              |")
-                print("|              |")
-                print("|______________|")
-                sleep(0.5)
-                print("you got:")
-                #(v) replace with something more efficient later
-                for c in cards:
-                    if c <= 75:
-                        print("common")
-                    elif c > 75 and c <= 100:
-                        print("uncommon")
-            else:
-                print("u really broke")
-        #booster pack
-        if pack == "2":
-            if money >= 15:
-                money -= 15
-                print("imma maek shop first sry")
-            else:
-                print("u kinda broke")
+        shop = input("1. buy packs - 2. sell cards: ")
+        if shop == "1":
+            pack = input("what pack do you want to open? 1. base pack (10 money), " + red + "2. booster pack (coming soon!)" + reset + "you have " + yellow + str(money) + reset + " money: ")
+            if pack == "1":
+                #base pack
+                if money >= 10:
+                    money -= 10
+                #(v) pack stuff
+                    print("opening pack...")
+                    #(v) find cards
+                    card1 = randint(1,100)
+                    card2 = randint(1,100)
+                    cards = [card1, card2]
+                    colors = []
+                    for c in cards:
+                        #(v) common (75%)
+                        if c <= 75:
+                            binder.append("common")
+                            colors.append(blue)
+                        #(v) uncommon (25%)
+                        elif c > 75 and c <= 100:
+                            binder.append("uncommon")
+                            colors.append(green)
+                    #card animation
+                    print(card1)
+                    packanim(reset)
+                    sleep(0.1)
+                    print(colors[1] + " ______________ ")
+                    print(colors[1] + "/" + colors[0] + "______________" + colors[1] + "\\")
+                    print(colors[0] + "/" + reset + "______________" + colors[0] + "\\")
+                    print(reset + "|              |")
+                    print("|              |")
+                    print("|              |")
+                    print("|              |")
+                    print("|              |")
+                    print("|              |")
+                    print("|              |")
+                    print("|              |")
+                    print("|              |")
+                    print("|              |")
+                    print("|              |")
+                    print("|              |")
+                    print("|______________|")
+                    sleep(0.5)
+                    print("you got:")
+                    #(v) replace with something more efficient later
+                    for c in cards:
+                        if c <= 75:
+                            print(blue + "common" + reset)
+                        elif c > 75 and c <= 100:
+                            print(green + "uncommon" + reset)
+                else:
+                    print("u really broke")
+            #booster pack
+            if pack == "2":
+                if money >= 15:
+                    money -= 15
+                    print("imma maek shop first sry")
+                else:
+                    print("u kinda broke")
 
-        elif pack != "1" and pack != "2":
-            print("typo, anyone?")
+            elif pack != "1" and pack != "2":
+                print("typo, anyone?")
+        #sell cards
+        if shop == "2":
+            common = 0
+            uncommon = 0
+            rare = 0
+            epic = 0
+            legendary = 0
+            mythic = 0
+            godlike = 0
+            for c in binder:
+                if c == "common":
+                    common += 1
+                elif c == "uncommon":
+                    uncommon += 1
+                elif c == "rare":
+                    rare += 1
+                elif c == "epic":
+                    epic += 1
+                elif c == "legendary":
+                    legendary += 1
+                elif c == "mythic":
+                    mythic += 1
+                elif c == "godlike":
+                    godlike += 1
+            sell = input("what card do you want to sell? you have 1. " + str(common) + " common (5 money), 2. " + str(uncommon) + " uncommon (7 money), 3. " + str(rare) + " rare (" + red + "unavailable" + reset + "), 4. " + str(epic) + " epic (" + red + "unavailable" + reset + "), 5. " + str(legendary) + " legendary (" + red + "unavailable" + reset + "), 6. " + str(mythic) + " mythic (" + red + "unavailable" + reset + "), 7. " + str(godlike) + " godlike: " + red + "unavailable" + reset + ": ")
+            if sell == "1": #(!) finish pls
+                num = input("how many commons do you want to sell for 6 money each? you have " + str(common) + ": ")
+                if int(num) > common:
+                    print("u cant count")
+                    print("sold all ur commons")
+                    money += common * 5
+                    for i in binder:
+                        if i == "common":
+                            binder.remove("common")
+                if int(num) <= common:
+                    print("sold " + str(num) + " commons")
+                    money += int(num) * 5
+                    for i in binder:
+                        if i == "common" and int(num) > 0:
+                            num = int(num) - 1
+                            binder.remove("common")
+
     elif menu == "2":
         print("you have " + str(len(binder)) + " cards in your binder")
-        print("you have" + str(money) + " money")
         for c in binder:
-            if c == "common":
+            if c =="common":
                 print(blue + c + reset)
             elif c == "uncommon":
                 print(green + c + reset)
@@ -397,12 +440,9 @@ while True:
                 print(red + c + reset)
             elif c == "godlike":
                 print(magenta + c + reset)
-    elif menu == "3":
-        print("no shop yet, i literally told you that sowhydidyputype3cuzISAIDYOUSHOULDNTTYPE3SONOWIMMADANDIWILLPUNISHYOUFORYOURACTIONTHISISWHATYOUDESERVED")
-        sleep(2)
-        print("lol")
-        exit()
+            else:
+                print("error")
     elif menu != "1" and menu != "2" and menu != "3":
-        print("bro cant type letters")
+        print("bro cant type numbers")
 #vNA
 
